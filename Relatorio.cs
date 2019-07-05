306aecc48e5bb92f049ce1915747b37e71881f66
Matheus Rodrigues <matheusuniversal@yahoo.com.br>
2019-07-05 18:13:31 -0300

    Commit inicial

---
+++ Relatorio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace web
+{
+    public class Relatorio
+    {
+        private readonly Catalogo catalogo;
+
+        public Relatorio(Catalogo catalogo)
+        {
+            this.catalogo = catalogo;
+        }
+
+        public async Task Imprimir(HttpContext context)
+        {
+
+            foreach (var livro in catalogo.GetLivros())
+            {
+                await context.Response.WriteAsync($"{livro.Codigo,-10}{livro.Nome,-40}{livro.Preco.ToString("c"),10}\r\n");
+            }
+        }
+    }
+}
